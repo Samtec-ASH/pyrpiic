76d13d38010c27e64b06e1a48ffc5bf0a65d7d1b
Adam Page <adam.page@samtec.com>
2020-01-06 22:10:21 -0500

    bug: Add missing gpio pins. feat: Enable reading gpio direction.

--- pyrpiic/ioexpander/tca6416a.py
+++ pyrpiic/ioexpander/tca6416a.py
@@ -10,8 +10,8 @@ class GPIODir(str, Enum):
 
 
 class TCA6416A:
-    PORT0 = ['P01', 'P02', 'P03', 'P04', 'P05', 'P06', 'P07']
-    PORT1 = ['P11', 'P12', 'P13', 'P14', 'P15', 'P16', 'P17']
+    PORT0 = ['P00', 'P01', 'P02', 'P03', 'P04', 'P05', 'P06', 'P07']
+    PORT1 = ['P10', 'P11', 'P12', 'P13', 'P14', 'P15', 'P16', 'P17']
     TCA6416A_BASE_INPUT = 0x00
     TCA6416A_PORT0_INPUT = 0x00
     TCA6416A_PORT1_INPUT = 0x01
@@ -80,6 +80,13 @@ class TCA6416A:
         if gpio in TCA6416A.PORT1:
             return TCA6416A.PORT1.index(gpio)
         raise ValueError(f'GPIO {gpio} is not a valid value')
+
+    def get_gpio_direction(self, gpio: str, gpio_dir: GPIODir):
+        ''' Get GPIO direction as either in or out. '''
+        port_index = self.get_port_index(gpio)
+        gpio_bit = self.get_gpio_bit_position(gpio)
+        value = self.get_register_bit(self.TCA6416A_BASE_CONFIG + port_index, gpio_bit)
+        return GPIODir.IN if value else GPIODir.OUT
 
     def set_gpio_direction(self, gpio: str, gpio_dir: GPIODir):
         ''' Set GPIO direction as either in or out. '''
