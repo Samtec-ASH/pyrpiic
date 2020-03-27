cf15918c302834206d406aea7446956640542382
Chris <chris.page@samtec.com>
2020-03-27 19:42:12 +0000

    fix: set i2c address for hdc1080

--- pyrpiic/sensor/hdc1080.py
+++ pyrpiic/sensor/hdc1080.py
@@ -57,6 +57,7 @@ class HDC1080:
     def configure(self):
         """ configure for acquisition mode """
         config = HDC1080_CONFIG_ACQUISITION_MODE
+        self.i2c.set_address(self.address)
         self.i2c_reg.write_register_bytes(HDC1080_CONFIGURATION_REGISTER,
                                           bytes(bytearray([config >> 8, 0x00])))
         time.sleep(0.015)
@@ -64,9 +65,11 @@ class HDC1080:
     def read_temperature(self) -> float:
         ''' read temperature and return a float '''
         s = bytes(bytearray([HDC1080_TEMPERATURE_REGISTER]))
+        self.i2c.set_address(self.address)
         self.i2c.write(s)
         time.sleep(0.0625)              # Required delay
 
+        self.i2c.set_address(self.address)
         data = self.i2c.read(2)  # read 2 byte temperature data
         buf = array.array('B', data)
         #print ( "Temp: %f 0x%X %X" % (  ((((buf[0]<<8) + (buf[1]))/65536.0)*165.0 ) - 40.0   ,buf[0],buf[1] )  )
@@ -80,6 +83,7 @@ class HDC1080:
         ''' read humidity and return a float '''
         time.sleep(0.015)               # Required delay
         s = bytes(bytearray([HDC1080_HUMIDITY_REGISTER]))
+        self.i2c.set_address(self.address)
         self.i2c.write(s)
         time.sleep(0.0625)              # Required delay
 
@@ -93,6 +97,7 @@ class HDC1080:
     def read_config_register(self) -> int:
         ''' read configuration register and return integer value '''
         s = bytes(bytearray([HDC1080_CONFIGURATION_REGISTER]))
+        self.i2c.set_address(self.address)
         self.i2c.write(s)
         time.sleep(0.0625)              # Required delay
 
@@ -109,6 +114,7 @@ class HDC1080:
         config = config | HDC1080_CONFIG_HEATER_ENABLE
         s = [HDC1080_CONFIGURATION_REGISTER, config >> 8, 0x00]
         s2 = bytes(bytearray(s))
+        self.i2c.set_address(self.address)
         self.i2c.write(s2)  # sending config register bytes
         time.sleep(0.015)               # Required delay
 
@@ -118,6 +124,7 @@ class HDC1080:
         config = config & ~HDC1080_CONFIG_HEATER_ENABLE
         s = [HDC1080_CONFIGURATION_REGISTER, config >> 8, 0x00]
         s2 = bytes(bytearray(s))
+        self.i2c.set_address(self.address)
         self.i2c.write(s2)  # sending config register bytes
         time.sleep(0.015)               # Required delay
 
@@ -127,6 +134,7 @@ class HDC1080:
         config = (config & ~0x0300) | resolution.value
         s = [HDC1080_CONFIGURATION_REGISTER, config >> 8, 0x00]
         s2 = bytes(bytearray(s))
+        self.i2c.set_address(self.address)
         self.i2c.write(s2)  # sending config register bytes
         time.sleep(0.015)               # Required delay
 
@@ -137,6 +145,7 @@ class HDC1080:
 
         s = [HDC1080_CONFIGURATION_REGISTER, config >> 8, 0x00]
         s2 = bytes(bytearray(s))
+        self.i2c.set_address(self.address)
         self.i2c.write(s2)  # sending config register bytes
         time.sleep(0.015)               # Required delay
 
@@ -152,6 +161,7 @@ class HDC1080:
         ''' get manufacturuer id (int) '''
         s = [HDC1080_MANUFACTURERID_REGISTER]  # temp
         s2 = bytes(bytearray(s))
+        self.i2c.set_address(self.address)
         self.i2c.write(s2)
         time.sleep(0.0625)              # Required delay
 
@@ -164,6 +174,7 @@ class HDC1080:
         ''' get device id (int) '''
         s = [HDC1080_DEVICEID_REGISTER]  # temp
         s2 = bytes(bytearray(s))
+        self.i2c.set_address(self.address)
         self.i2c.write(s2)
         time.sleep(0.0625)              # Required delay
 
@@ -178,6 +189,7 @@ class HDC1080:
 
         s = [HDC1080_SERIALIDHIGH_REGISTER]  # temp
         s2 = bytes(bytearray(s))
+        self.i2c.set_address(self.address)
         self.i2c.write(s2)
         time.sleep(0.0625)              # Required delay
         data = self.i2c.read(2)  # read 2 byte config data
